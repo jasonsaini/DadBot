10e52e208c33fd93877e77624e474d5252f3bb86
jasonsaini <69808698+jasonsaini@users.noreply.github.com>
2021-03-01 14:22:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,39 @@ from dadjokes import Dadjoke
 from pyowm import OWM
 from dotenv import load_dotenv
 
-# Load tokens from .env
 load_dotenv('token.env')
 
 client = discord.Client()
-
-# Initialize tokens 
 WEATHER_TOKEN = os.getenv('WEATHER_TOKEN')
 owm = OWM(WEATHER_TOKEN)
 mgr = owm.weather_manager()
-
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-client.run(DISCORD_TOKEN)
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data = json.loads(response.text)
   quote = json_data[0]['q'] + "-" + json_data[0]['a']
   return quote
+
+def get_weather(message):
+  location = " "
+  words = message.content.split(" ")
+  word_count = 0
+  for i in words:
+    word_count += 1
+    if i == "in":
+      location = words[word_count]
+      if '?' in location:
+        location = location[:-1]
+
+  observation = mgr.weather_at_place(location)
+  w = observation.weather
+  weather = w.detailed_status
+  temperature_dict = w.temperature('fahrenheit')
+  temperature = temperature_dict.get('temp')
+  location = location.capitalize()
+  weather = weather.capitalize()
+  return {'location': location, 'temperature': temperature, 'weather': weather}
+
 
 @client.event
 async def on_ready():
@@ -34,7 +49,7 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-  # Lowercase message for reading
+
   message.content = message.content.lower()
 
   #greets user
@@ -58,27 +73,8 @@ async def on_message(message):
   
   # tells weather
   if 'weather' in message.content:
-    location = " "
-    words = message.content.split(" ")
-    word_count = 0
-    # Gets location from message
-    for i in words:
-      word_count += 1
-      if i == "in":
-        location = words[word_count]
-        # Remove question mark from string
-        if '?' in location:
-          location = location[:-1]
-
-  # Gets information from weather API      
-    observation = mgr.weather_at_place(location)
-    w = observation.weather
-    weather = w.detailed_status
-    temperature_dict = w.temperature('fahrenheit')
-    temperature = temperature_dict.get("temp")
-    location = location.capitalize()
-    weather = weather.capitalize()
-    await message.channel.send(f"{location} : {weather} and it's {temperature}\xb0F today!")
+    weather_dict = get_weather(message)
+    await message.channel.send(f"{weather_dict['location']} : {weather_dict['weather']} and it's {weather_dict['temperature']}\xb0F today!")
     return
 
   # Sends help information to user
@@ -91,3 +87,6 @@ async def on_message(message):
     quote = get_quote()
     await message.channel.send(quote)
     return
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+client.run(DISCORD_TOKEN)
